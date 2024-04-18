5a6df3e9ff118835b3ec065605aecca46e1a51c2
JhonatanRv <jhonatan_r_v@outlook.com>
2024-04-17 22:34:26 -0300

    Login and signup screen

---
+++ screens/Login.js
@@ -0,0 +1,130 @@
+import { View, Text, Pressable, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from "react-native-safe-area-context";
+import COLORS from '../constants/colors';
+import { Ionicons } from "@expo/vector-icons";
+import Button from '../components/Button';
+
+const Login = ({ navigation }) => {
+    const [isPasswordShown, setIsPasswordShown] = useState(false);
+    
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
+            <View style={{ flex: 1, marginHorizontal: 22 }}>
+                <View style={{ marginVertical: 22 }}>
+                    <Text style={{
+                        fontSize: 22,
+                        fontWeight: 'bold',
+                        marginVertical: 12,
+                        color: COLORS.black
+                    }}>
+                        Bem-vindo(a) de volta ! 
+                    </Text>
+
+                </View>
+
+                <View style={{ marginBottom: 12 }}>
+                    <Text style={{
+                        fontSize: 16,
+                        fontWeight: 400,
+                        marginVertical: 8
+                    }}>Endereço de Email</Text>
+
+                    <View style={{
+                        width: "100%",
+                        height: 48,
+                        borderColor: COLORS.black,
+                        borderWidth: 1,
+                        borderRadius: 8,
+                        alignItems: "center",
+                        justifyContent: "center",
+                        paddingLeft: 22
+                    }}>
+                        <TextInput
+                            placeholder='Escreva seu endereço de Email'
+                            placeholderTextColor={COLORS.black}
+                            keyboardType='email-address'
+                            style={{
+                                width: "100%"
+                            }}
+                        />
+                    </View>
+                </View>
+
+                <View style={{ marginBottom: 12 }}>
+                    <Text style={{
+                        fontSize: 16,
+                        fontWeight: 400,
+                        marginVertical: 8
+                    }}>Senha</Text>
+
+                    <View style={{
+                        width: "100%",
+                        height: 48,
+                        borderColor: COLORS.black,
+                        borderWidth: 1,
+                        borderRadius: 8,
+                        alignItems: "center",
+                        justifyContent: "center",
+                        paddingLeft: 22
+                    }}>
+                        <TextInput
+                            placeholder='Escreva sua senha'
+                            placeholderTextColor={COLORS.black}
+                            secureTextEntry={isPasswordShown}
+                            style={{
+                                width: "100%"
+                            }}
+                        />
+
+                        <TouchableOpacity
+                            onPress={() => setIsPasswordShown(!isPasswordShown)}
+                            style={{
+                                position: "absolute",
+                                right: 12
+                            }}
+                        >
+                            {
+                                isPasswordShown == true ? (
+                                    <Ionicons name="eye-off" size={24} color={COLORS.black} />
+                                ) : (
+                                    <Ionicons name="eye" size={24} color={COLORS.black} />
+                                )
+                            }
+
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                <Button
+                    title="Entrar"
+                    filled
+                    style={{
+                        marginTop: 18,
+                        marginBottom: 4,
+                    }}
+                />
+
+                <View style={{
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    marginVertical: 22
+                }}>
+                    <Text style={{ fontSize: 16, color: COLORS.black }}>Não possui uma conta ? </Text>
+                    <Pressable
+                        onPress={() => navigation.navigate("Signup")}
+                    >
+                        <Text style={{
+                            fontSize: 16,
+                            color: COLORS.primary,
+                            fontWeight: "bold",
+                            marginLeft: 6
+                        }}>Cadastrar</Text>
+                    </Pressable>
+                </View>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default Login
---
+++ screens/Welcome.js
@@ -0,0 +1,102 @@
+import { View, Text, Pressable, Image } from 'react-native'
+import React from 'react'
+import { LinearGradient } from "expo-linear-gradient";
+import COLORS from '../constants/colors';
+import Button from '../components/Button';
+
+const Welcome = ({ navigation }) => {
+
+    return (
+        <LinearGradient
+            style={{
+                flex: 1
+            }}
+            colors={[COLORS.secondary, COLORS.primary]}
+        >
+            <View style={{ flex: 1}}>
+                <View style = {{ marginHorizontal: -80, marginVertical: -20 }}>
+                
+
+                    <Image
+                        source={require("../assets/TechMart.png")}
+                        style={{
+                            height: 240,
+                            width: 240,
+                            borderRadius: 20,
+                            position: "absolute",
+                            top: 110,
+                            left: 100,
+                            transform: [
+                                { translateX: 50 },
+                                { translateY: 50 },
+                                
+                            ]
+                        }}
+                    />
+                </View>
+
+                {/* content  */}
+
+                <View style={{
+                    paddingHorizontal: 22,
+                    position: "absolute",
+                    top: 400,
+                    width: "100%"
+                }}>
+                    <Text style={{
+                        fontSize: 50,
+                        fontWeight: 800,
+                        color: COLORS.white
+                    }}>Vamos</Text>
+                    <Text style={{
+                        fontSize: 46,
+                        fontWeight: 800,
+                        color: COLORS.white
+                    }}>Começar</Text>
+
+                    <View style={{ marginVertical: 22 }}>
+                        <Text style={{
+                            fontSize: 16,
+                            color: COLORS.white,
+                            marginVertical: 4
+                        }}>Entre agora ou crie uma nova conta para ver os produtos da TechMart!</Text>
+                        
+                    </View>
+
+                    <Button
+                        title="Criar uma conta"
+                        onPress={() => navigation.navigate("Signup")}
+                        style={{
+                            marginTop: 22,
+                            width: "100%"
+                        }}
+                    />
+
+                    <View style={{
+                        flexDirection: "row",
+                        marginTop: 12,
+                        justifyContent: "center"
+                    }}>
+                        <Text style={{
+                            fontSize: 16,
+                            color: COLORS.white
+                        }}>Já possui uma conta ?</Text>
+                        <Pressable
+                            onPress={() => navigation.navigate("Login")}
+                        >
+                            <Text style={{
+                                fontSize: 16,
+                                color: COLORS.white,
+                                fontWeight: "bold",
+                                marginLeft: 4
+                            }}>Entrar</Text>
+                        </Pressable>
+
+                    </View>
+                </View>
+            </View>
+        </LinearGradient>
+    )
+}
+
+export default Welcome
